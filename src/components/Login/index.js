31bf069aa048a71ea61e00b9155b04a845d0c510
vivekrudra980 <131126507+vivekrudra980@users.noreply.github.com>
2024-06-07 23:06:37 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -26,8 +26,8 @@ class Login extends Component {
     event.preventDefault()
     let {username, password} = this.state
 
-    if (username.toLowerCase().trim(' ') === 'santosh') username = 'rahul'
-    if (password === 'santosh@2023') password = 'rahul@2021'
+    if (username.toLowerCase().trim(' ') === 'vivek') username = 'rahul'
+    if (password === 'vivek@2023') password = 'rahul@2021'
 
     const userDetails = {username, password}
     const LoginApiUrl = 'https://apis.ccbp.in/login'
@@ -60,7 +60,7 @@ class Login extends Component {
           type="text"
           value={username}
           className="login-input-field"
-          placeholder="santosh"
+          placeholder="Enter User Name"
           id="username"
           onChange={this.updateUsername}
         />
@@ -79,7 +79,7 @@ class Login extends Component {
           type="password"
           value={password}
           className="login-input-field"
-          placeholder="santosh@2023"
+          placeholder="Enter Password"
           id="password"
           onChange={this.updatePassword}
         />
